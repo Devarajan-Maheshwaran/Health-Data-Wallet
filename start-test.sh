6bf18c375c5adc0b665f48cdcf1feae8aebc3333
Devarajan-Maheshwaran <devarajanm.magesh@gmail.com>
2025-07-14 16:35:08 +0530

    deleted unnecessary files

--- start-test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Starting test server..."
-node test-simple-server.js
--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Start our application using Node.js
-echo "Starting application with Node.js..."
-node run.js
